66454f5ec1294afa919a4afd4273c2f51cf72baa
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2023-09-17 00:18:32 +0530

    Update CountSort.cpp

--- CountSort.cpp
+++ CountSort.cpp
@@ -1,42 +1,51 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-void CountSort(int arr[],int n){
-    int k;
-    k=arr[0];
-    for(int i=0;i<n;i++){
-        k=max(k,arr[i]);
+void countingSort(vector<int>& arr) {
+    if (arr.empty()) {
+        return; // No need to sort an empty array
     }
-    int count[10]={0};
-    for(int i=0;i<n;i++){
+
+    // Find the maximum element in the array to determine the range
+    int max_element = *max_element(arr.begin(), arr.end());
+
+    // Create a count array and initialize it with zeros
+    vector<int> count(max_element + 1, 0);
+
+    // Count the occurrences of each element in the input array
+    for (int i = 0; i < arr.size(); ++i) {
         count[arr[i]]++;
     }
-    for(int i=1;i<=k;i++){
-        count[i]+=count[i-1];
-    }
-    int output[n];
-    for(int i=n-1;i>=0;i--){
-        output[--count[arr[i]]]=arr[i];
-    }
-    for(int i=0;i<n;i++){
-        arr[i]=output[i];
+
+    // Reconstruct the sorted array from the count array
+    int index = 0;
+    for (int i = 0; i <= max_element; ++i) {
+        while (count[i] > 0) {
+            arr[index] = i;
+            index++;
+            count[i]--;
+        }
     }
 }
 
-int main(){
-    int n;
-    cout<<"Enter size of array=";
-    cin>>n;
-    int arr[n];
-    int i,j;
-    cout<<"Enter elements in an array=";
-    for(i=0;i<n;i++){
-        cin>>arr[i];
+int main() {
+    vector<int> arr = {4, 2, 2, 8, 3, 3, 1};
+
+    cout << "Original Array: ";
+    for (int num : arr) {
+        cout << num << " ";
     }
-    CountSort(arr,n);
-    cout<<"Final sorted array is:"<<endl;
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    cout << endl;
+
+    countingSort(arr);
+
+    cout << "Sorted Array: ";
+    for (int num : arr) {
+        cout << num << " ";
     }
+    cout << endl;
+
     return 0;
 }
